29bc889d4579c603bd7e93e535ee72d1a5efdb2a
A H <>
2020-05-03 22:41:42 -0700

    logic complete, continue refactor

--- main.js
+++ main.js
@@ -5,19 +5,18 @@ localStorage.clear();
 
 
 let imageList = [
-  // 'https://learn.g2.com/hs-fs/hubfs/plan%20gif%20marketing%20strategy.gif?width=500&name=plan%20gif%20marketing%20strategy.gif',
   'https://media.giphy.com/media/SKGo6OYe24EBG/giphy.gif',
   'https://media.giphy.com/media/vB5jiVv1Tr2tW/giphy.gif',
-  // 'https://media.giphy.com/media/IzVwOO8xZsfks/giphy.gif',  
-  // 'https://media.giphy.com/media/5Ams0SJhK7Vq8/giphy.gif',
-  // 'https://media.giphy.com/media/UvOcKPHrkKSLm/giphy.gif',
-  // 'https://media.giphy.com/media/D7z8JfNANqahW/giphy.gif',
-  // 'https://media.giphy.com/media/26ufnwz3wDUli7GU0/giphy.gif',
-  // 'https://media.giphy.com/media/3oriNZoNvn73MZaFYk/giphy.gif',
-  // 'https://media.giphy.com/media/nrXif9YExO9EI/giphy.gif',
-  // 'https://media.giphy.com/media/lvzdeWk12qjmM/giphy.gif',
-  // 'https://media.giphy.com/media/ISOckXUybVfQ4/giphy.gif',
-  // 'https://media.giphy.com/media/4no7ul3pa571e/giphy.gif'
+  'https://media.giphy.com/media/IzVwOO8xZsfks/giphy.gif',
+  'https://media.giphy.com/media/5Ams0SJhK7Vq8/giphy.gif',
+  'https://media.giphy.com/media/UvOcKPHrkKSLm/giphy.gif',
+  'https://media.giphy.com/media/D7z8JfNANqahW/giphy.gif',
+  'https://media.giphy.com/media/26ufnwz3wDUli7GU0/giphy.gif',
+  'https://media.giphy.com/media/3oriNZoNvn73MZaFYk/giphy.gif',
+  'https://media.giphy.com/media/nrXif9YExO9EI/giphy.gif',
+  'https://media.giphy.com/media/lvzdeWk12qjmM/giphy.gif',
+  'https://media.giphy.com/media/ISOckXUybVfQ4/giphy.gif',
+  'https://media.giphy.com/media/4no7ul3pa571e/giphy.gif'
 ]
 
 
@@ -26,12 +25,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
   startBtn.addEventListener('click', (e) => {
     // console.log('clicked', e.target);
-    gameArea.scrollIntoView();
-
+    // reset for subsequent rounds
     cardList.innerHTML = "";
     numMatch = 0;
+    numFlipped = 0;
 
     startGame();
+    gameArea.scrollIntoView();
   });
 });
 
@@ -41,36 +41,38 @@ function startGame() {
   list = list.concat(list);
   shuffleList(list);
 
+  // first half
   for (let i = 0; i < list.length / 2; i++) {
-    let card = createCard(list, i);
+    let card = createCard(list[i]);
     cardList.appendChild(card);
   }
 
+  // add score keeper
   let score = document.createElement('div');
-  // score.setAttribute('class', 'card');
   score.setAttribute('id', 'score');
   score.value = 0;
   score.innerHTML = `${score.value}`;
   cardList.appendChild(score);
 
+  // second half
   for (let i = list.length / 2; i < list.length; i++) {
-    let card = createCard(list, i);
+    let card = createCard(list[i]);
     cardList.appendChild(card);
   }
 }
 
-function createCard(list, i) {
+function createCard(indexInShuffledList) {
   let card = document.createElement('div');
   card.setAttribute('class', 'card');
   // card.setAttribute('class', 'flipped');
-  card.dataset['value'] = list[i];
+  card.dataset['value'] = indexInShuffledList;
 
-
-  let image = imageList[list[i]];
+  let image = imageList[indexInShuffledList];
 
   // for debug
-  card.innerHTML = `${list[i]}<img src=${image} class='img'>`;
-  // card.innerHTML = `<img src=${image} class='img'>`;
+  // card.innerHTML = `${indexInShuffledList}<img src=${image} class='img'>`;
+
+  card.innerHTML = `<img src=${image} class='img'>`;
 
   card.addEventListener('click', flip);
   return card;
@@ -78,68 +80,72 @@ function createCard(list, i) {
 
 function flip(e) {
   // console.log(e, e.target, this);
-  if(numFlipped === 2){
+  if (numFlipped === 2) {
     console.log('already clicked 2 cards, wait for match');
+    return;
+  }
+  // console.log(e.target.parentNode, e.target.classList, e.target.classList.contains('done'), numFlipped);
+  // check if parent (card) is already done
+  if (e.target.parentNode.classList.contains('done')) {
+    console.log('clicked an already matched card, input ignored');
+    return;
+  }
+  // check if same card
+  if (e.target === this.querySelector('.img')) {
+    console.log('clicked same card, input ignored');
     return;
   }
 
   numFlipped++;
-  if(numFlipped < 2){
+  if (numFlipped < 2) {
     this.classList.toggle('flipped');
     score.innerHTML = `${++score.value}`;
   }
-  else {
-    if (e.target === this.querySelector('.img'))
-      console.log('clicked same card, input ignored');
-    else { //2 different cards clicked
-      score.innerHTML = `${++score.value}`;
-      this.classList.toggle('flipped');
-      checkIfMatch();
-    }
+  else {//2 different cards clicked
+    score.innerHTML = `${++score.value}`;
+    this.classList.toggle('flipped');
+    checkIfMatch();
   }
-
 }
 
 function checkIfMatch() {
-  backs = document.querySelectorAll('.flipped');
-  if (backs.length > 1) {
-    backs = [...backs];
-    
-    // debug
-    // backs.forEach((el) => console.log(el.dataset));
-    if (backs[0].dataset['value'] === backs[1].dataset['value']) {
-      console.log('match found');
-      numMatch++;
-      
-      
-      console.log("numMatch:", numMatch);
-      if(numMatch === imageList.length){
-        console.log('all matches found, game complete');
+  let flippedCards = document.querySelectorAll('.flipped');
+  flippedCards = [...flippedCards];
+  // flippedCards.forEach((el) => console.log(el.dataset));
 
-        let lscore = +localStorage['score'] || Infinity;
-        if(score.value < lscore) localStorage['score'] = score.value;
-        bestScore.innerHTML = `best score: ${localStorage['score']}`;
-      }
-      setTimeout(() => {
-        backs.forEach((el) => {
-          el.classList.remove('flipped');
-          el.classList.add('done');
-          numFlipped = 0;
-        });
-      }, 0);
+  if (flippedCards[0].dataset['value'] === flippedCards[1].dataset['value']) {
+    console.log('match found');
+    numMatch++;
+
+    console.log("numMatch:", numMatch);
+    if (numMatch === imageList.length) {
+      console.log('all matches found, game complete');
+
+      let lscore = +localStorage['score'] || Infinity;
+      if (score.value < lscore) localStorage['score'] = score.value;
+      bestScore.innerHTML = `best score: ${localStorage['score']}`;
     }
-    else {
-      console.log('not a match');
-      setTimeout(() => {
-        backs.forEach((el) => el.classList.remove('flipped'));
-        numFlipped = 0;
-      }, 1000);
-    }
+
+    flippedCards.forEach((el) => {
+      el.classList.remove('flipped');
+      el.classList.add('done');
+      numFlipped = 0;
+    });
+
+  }
+  else {
+    console.log('not a match');
+
+    // timeout currently necessary for animation
+    setTimeout(() => {
+      flippedCards.forEach((el) => el.classList.remove('flipped'));
+      numFlipped = 0;
+    }, 1000);
   }
 }
 
+// utility functions:
 // from shuffle/sample at: https://underscorejs.org/docs/underscore.html
-// and https://stackoverflow.com/a/46161940
 function random(min, max) {
   if (max == null) {
     max = min;
